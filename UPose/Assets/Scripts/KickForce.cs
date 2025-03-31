83369f36a80b771802b25d53a9414fc4cdc2561f
Angelos Barmpoutis <angelbar@ufl.edu>
2025-03-31 16:24:46 -0400

    Implemented collision detection

---
+++ UPose/Assets/Scripts/KickForce.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class KickForce : MonoBehaviour
+{
+    public float kickForce = 10f; 
+    private Rigidbody footRb; 
+
+    void Start()
+    {
+        
+        footRb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collided");
+
+        if (collision.gameObject.CompareTag("Box"))
+        {
+            Rigidbody boxRb = collision.gameObject.GetComponent<Rigidbody>();
+            if (boxRb != null && footRb != null)
+            {
+                float forceAmount = footRb.linearVelocity.magnitude * kickForce;
+
+                Vector3 kickDirection = collision.contacts[0].point - transform.position;
+                kickDirection = kickDirection.normalized;
+
+                boxRb.AddForce(kickDirection * forceAmount, ForceMode.Impulse);
+            }
+        }
+    }
+}
--- UPose/Assets/Scripts/ReadyPlayerAvatar.cs
+++ UPose/Assets/Scripts/ReadyPlayerAvatar.cs
@@ -90,12 +90,31 @@ public class ReadyPlayerAvatar : MonoBehaviour
             RightLeg = GetComponentsInChildren<Transform>(true).FirstOrDefault(t => t.name == "RightLeg");
             
             Transform LeftFoot=GetComponentsInChildren<Transform>(true).FirstOrDefault(t => t.name == "LeftFoot");
-            Rigidbody rb=LeftFoot.gameObject.AddComponent<Rigidbody>();
+            
+            GameObject colliderHolder = new GameObject("LeftFootCollider");
+            colliderHolder.transform.SetParent(LeftFoot);
+            colliderHolder.transform.localPosition = new Vector3(0, 0.125f, 0);
+            colliderHolder.transform.localRotation = Quaternion.Euler(-55,0,0);
+            Rigidbody rb=colliderHolder.AddComponent<Rigidbody>();
             rb.isKinematic=true;
+            rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+            BoxCollider footCollider = colliderHolder.AddComponent<BoxCollider>();
+            footCollider.size = new Vector3(0.15f, 0.1f, 0.3f);
+            colliderHolder.AddComponent<KickForce>();
+
+
             Transform RightFoot=GetComponentsInChildren<Transform>(true).FirstOrDefault(t => t.name == "RightFoot");
-            rb=RightFoot.gameObject.AddComponent<Rigidbody>();
+        
+            colliderHolder = new GameObject("RightFootCollider");
+            colliderHolder.transform.SetParent(RightFoot);
+            colliderHolder.transform.localPosition = new Vector3(0, 0.125f, 0);
+            colliderHolder.transform.localRotation = Quaternion.Euler(-55,0,0);
+            rb=colliderHolder.AddComponent<Rigidbody>();
             rb.isKinematic=true;
-             
+            rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+            footCollider = colliderHolder.AddComponent<BoxCollider>();
+            footCollider.size = new Vector3(0.15f, 0.1f, 0.3f);
+            colliderHolder.AddComponent<KickForce>();
 
             LeftShoulder = GetComponentsInChildren<Transform>(true).FirstOrDefault(t => t.name == "LeftShoulder");
             
@@ -103,12 +122,37 @@ public class ReadyPlayerAvatar : MonoBehaviour
             LeftArm.localRotation=Quaternion.Euler(0,0,0);
             LeftForeArm = GetComponentsInChildren<Transform>(true).FirstOrDefault(t => t.name == "LeftForeArm");
             
+            Transform LeftHand=GetComponentsInChildren<Transform>(true).FirstOrDefault(t => t.name == "LeftHand");
+        
+            colliderHolder = new GameObject("LeftHandCollider");
+            colliderHolder.transform.SetParent(LeftHand);
+            colliderHolder.transform.localPosition = new Vector3(0, 0.1f, 0);
+            colliderHolder.transform.localRotation = Quaternion.Euler(-90,0,0);
+            rb=colliderHolder.AddComponent<Rigidbody>();
+            rb.isKinematic=true;
+            rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+            footCollider = colliderHolder.AddComponent<BoxCollider>();
+            footCollider.size = new Vector3(0.15f, 0.1f, 0.2f);
+            colliderHolder.AddComponent<KickForce>();
+
             RightShoulder = GetComponentsInChildren<Transform>(true).FirstOrDefault(t => t.name == "RightShoulder");
             
             RightArm = GetComponentsInChildren<Transform>(true).FirstOrDefault(t => t.name == "RightArm");
             RightArm.localRotation=Quaternion.Euler(0,0,0);
             RightForeArm = GetComponentsInChildren<Transform>(true).FirstOrDefault(t => t.name == "RightForeArm");
             
+            Transform RightHand=GetComponentsInChildren<Transform>(true).FirstOrDefault(t => t.name == "RightHand");
+        
+            colliderHolder = new GameObject("RightHandCollider");
+            colliderHolder.transform.SetParent(RightHand);
+            colliderHolder.transform.localPosition = new Vector3(0, 0.1f, 0);
+            colliderHolder.transform.localRotation = Quaternion.Euler(-90,0,0);
+            rb=colliderHolder.AddComponent<Rigidbody>();
+            rb.isKinematic=true;
+            rb.collisionDetectionMode = CollisionDetectionMode.ContinuousDynamic;
+            footCollider = colliderHolder.AddComponent<BoxCollider>();
+            footCollider.size = new Vector3(0.15f, 0.1f, 0.2f);
+            colliderHolder.AddComponent<KickForce>();
             
             AVATAR_LOADED=true;
 
